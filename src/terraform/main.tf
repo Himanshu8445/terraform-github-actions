99c774e044aaaef3aa252b55a64bb2877c60c076
himanshusharma-osv_fedex <himanshusharma.osv@fedex.com>
2022-11-23 15:29:08 +0530

    re-run

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -7,15 +7,15 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-}
-
 backend "azurerm" {
     resource_group_name  = "tftstaterg"
     storage_account_name = "tfstatestracc"
     container_name       = "tfstate-container"
     key                  = "test.tfstate"
+}
+
+provider "azurerm" {
+  features {}
 }
 
 resource "azurerm_resource_group" "this" {
